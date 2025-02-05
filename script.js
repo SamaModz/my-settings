22c1a886f0df10d6b6e4393679eff31a48d1d390
SamaModz <henriqueorlando46@gmail.com>
2025-02-04 23:25:50 -0300

    Update UI by Sama Modz

--- script.js
+++
@@ -1,5 +0,0 @@
-
-/* Project Created By Sama Modz */
-setInterval(function () {
-  document.getElementById('hours').innerText = new Date().toLocaleTimeString([], { hour: '2-digit', minute: '2-digit', hour12: false }); // Formato 24h
-}, 1000);
